368ba06a742e214360228dd018918bcdf60b9b32
rodrigoms95 <rodrigoms95@gmail.com>
2024-07-30 13:32:35 -0600

    xvbxfgb

--- code/CDF_calc.py
+++ code/CDF_calc.py
@@ -19,36 +19,36 @@ dims = [ "XTIME", "XLAT", "XLONG" ]
 # 2 futuro CHIRPS
 # 3 observado ERA 5
 model = sys.argv[1]
-if model == 0:
+if model == "0":
     path_d = "../data/CHIRPS_megalopolis.nc"
     path_r = "../temp/CHIRPS_megalopolis_cdf.nc"
-elif model == 1:
+elif model == "1":
     path_d = "../temp/WRF_regrid_CHIRPS_days.nc"
     path_r = "../temp/WRF_regrid_CHIRPS_days_cdf.nc"
-elif model == 2:
+elif model == "2":
     path_d = "../temp/WRF_2040_regrid_CHIRPS_days.nc"
     path_r = "../temp/WRF_2040_regrid_CHIRPS_days_cdf.nc"
-elif model == 3:
-    path_d = "../../Datos/ERA5_land/era5_land_total-precipitation.nc"
-    path_r = "../../temp/cc_idt/era5_land_total-precipitation_cdf.nc"
+elif model == "3":
+    path_d = "../../Datos/era5-land/era5-land_total-precipitation.nc"
+    path_r = "../../temp/cc_idt/era5-land_total-precipitation_cdf.nc"
 
 # Abrimos el archivo
 with xr.open_dataset(path_d) as ds:
     # Homologamos nombres
-    if model == 0:
+    if model == "0":
         ds = ds.rename( {"time": "XTIME", "longitude": "XLONG", 
             "latitude": "XLAT", "precip": "Pcp"})
-    elif model == 3: 
-        ds = ds.rename_vars( { list(ds.keys()): v,
+    elif model == "3": 
+        ds = ds.rename_vars( { list(ds.keys())[0]: v,
             "time": dims[0], "lat": dims[1], "lon": dims[2] }
             ).swap_dims( { "time": dims[0], "lat": dims[1], "lon": dims[2] } )
 
     # Reordenamos variables
     ds = ds[dims + [v]]
     # Creamos un dataset sin la variable
-    ds_2 = ds.drop_vars( list(ds.keys()) ).copy()
+    #ds_2 = ds.drop_vars( list(ds.keys()) ).copy()
     # Creamos un dataframe
-    df = ds.to_dataframe().sort_index()
+    df = ds.to_dataframe()#.sort_index()
 
     # Lista de coordenadas
     latitude = df.index.get_level_values(dims[1]).unique()
@@ -67,7 +67,8 @@ with xr.open_dataset(path_d) as ds:
             df.loc[ (slice(None), lat, lon), "q_model" ] = df_c["q_model"]
 
     # Covertimos a dataset
-    ds_2["q_model"] = df["q_model"].to_xarray()
-    ds["q_model"] = ( dims, ds_2["q_model"].values )
+    ds["q_model"] = ( dims, df["q_model"].to_xarray().values )
+    #ds_2["q_model"] = df["q_model"].to_xarray()
+   # ds["q_model"] = ( dims, ds_2["q_model"].values )
     # Guardamos el archivo.
     ds.to_netcdf( path_r, mode = "w" )
